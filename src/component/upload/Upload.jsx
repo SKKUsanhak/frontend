af3627e635085fbff2d230da62e0b09487fb65df
codingosol <codingosol@gmail.com>
2024-07-17 10:24:13 +0900

    LineFont Added

--- src/component/upload/Upload.jsx
+++ src/component/upload/Upload.jsx
@@ -2,22 +2,27 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './Upload.css';
+import { FadeLoader } from 'react-spinners';
 
 export default function Upload() {
     const [file, setFile] = useState(null);
     const [message, setMessage] = useState('');
     const [progress, setProgress] = useState(0);
+    const [progressMessage, setProgressMessage] = useState('');
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     const handleFileChange = (event) => {
         setFile(event.target.files[0]);
         setProgress(0); // 파일 변경 시 진행률 초기화
+        setProgressMessage('');
     };
 
     const handleDrop = (event) => {
         event.preventDefault();
         setFile(event.dataTransfer.files[0]);
         setProgress(0); // 파일 드롭 시 진행률 초기화
+        setProgressMessage('');
     };
 
     const handleDragOver = (event) => {
@@ -30,43 +35,33 @@ export default function Upload() {
             return;
         }
 
-        setMessage('업로드 중이니 잠시만 기다려 주세요 10~20초 가량 소모됩니다.');
+        setMessage('잠시만 기다려 주세요, 파일 크기에 따라 10~20초 가량 소모됩니다.');
+        setLoading(true); // 로딩 상태 활성화
+
         const formData = new FormData();
         formData.append('file', file);
-
+        setProgressMessage('파일 업로드 성공, Azure 분석 중...');
+        setProgress(30);
         axios.post('/upload', formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             },
-            responseType: 'arraybuffer',
-            onUploadProgress: (progressEvent) => {
-                const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-                setProgress(percentCompleted);
-            }
+            responseType: 'arraybuffer'
         })
         .then(response => {
-            setMessage('파일 업로드에 성공했습니다!');
-            navigate('/excelEditor', { state: { fileData: response.data } });
+            setMessage('파일 분석에 성공했습니다!');
+            setProgress(100);
+            setProgressMessage("분석 성공!");
+            setTimeout(() => {
+                setLoading(false); // 로딩 상태 비활성화
+                navigate('/excelEditor', { state: { fileData: response.data } });
+            }, 500); // 0.5초 뒤에 리다이렉션
         })
         .catch(error => {
-            setMessage('파일 업로드에 실패했습니다.');
-            console.error('파일 업로드 중 오류가 발생했습니다!', error);
+            setMessage('파일 분석에 실패했습니다.');
+            setLoading(false); // 로딩 상태 비활성화
         });
 
-         // 진행 상황을 폴링하여 업데이트
-         const intervalId = setInterval(() => {
-            axios.get('/progress')
-                .then(response => {
-                    setProgress(response.data);
-                    if (response.data === 100) {
-                        clearInterval(intervalId);
-                    }
-                })
-                .catch(error => {
-                    console.error('진행 상황 업데이트 중 오류가 발생했습니다!', error);
-                    clearInterval(intervalId);
-                });
-        }, 1000); // 1초 간격으로 진행 상황 업데이트
     };
 
     return (
@@ -92,15 +87,12 @@ export default function Upload() {
                 <p className="drop-text">{file ? file.name : '또는 여기에 파일을 드롭하세요'}</p>
             </div>
             <button onClick={handleUpload} className="upload-button">업로드</button>
-            <div className="progress-bar" style={{ width: '400px' }}>
-                <div 
-                    className="progress-bar-fill" 
-                    style={{ width: `${progress}%` }}
-                >
-                    {progress}%
-                </div>
+            <div className={`loader-overlay ${loading ? '' : 'hidden'}`}>
+                <FadeLoader />
+                <p className="message">{message}</p>
+                <p className="progress-message">{progressMessage}</p>
+                <p>{progress}%</p>
             </div>
-            <p className="message">{message}</p>
         </div>
     );
 }
