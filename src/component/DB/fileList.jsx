643a88a49ac2b59c56a0e2ab459921a6c3145942
codingosol <codingosol@gmail.com>
2024-07-23 10:44:49 +0900

    Add fileList sort

--- src/component/DB/fileList.jsx
+++ src/component/DB/fileList.jsx
@@ -3,10 +3,12 @@ import axios from 'axios';
 import './db.css';
 import { FaTrash, FaEdit } from "react-icons/fa";
 import { IoIosSave } from "react-icons/io";
+import { GoTriangleDown, GoTriangleUp, GoTriangleRight } from "react-icons/go";
 
 export default function FileList({ files, selectedFileId, onFileSelect, onFileDelete, fetchFiles }) {
     const [editingFileId, setEditingFileId] = useState(null);
     const [newFileName, setNewFileName] = useState('');
+    const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
 
     const formatDate = (dateString) => {
         const options = { 
@@ -44,6 +46,38 @@ export default function FileList({ files, selectedFileId, onFileSelect, onFileDe
         }
     };
 
+    const handleSort = (key) => {
+        let direction = 'ascending';
+        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending';
+        } else if (sortConfig.key === key && sortConfig.direction === 'descending') {
+            key = null;
+            direction = null;
+        }
+        setSortConfig({ key, direction });
+    };
+
+    const sortedFiles = () => {
+        if (!sortConfig.key) return files;
+        const sorted = [...files].sort((a, b) => {
+            if (a[sortConfig.key] < b[sortConfig.key]) return sortConfig.direction === 'ascending' ? -1 : 1;
+            if (a[sortConfig.key] > b[sortConfig.key]) return sortConfig.direction === 'ascending' ? 1 : -1;
+            return 0;
+        });
+        return sorted;
+    };
+
+    const getSortIcon = (key) => {
+        if (sortConfig.key === key) {
+            if (sortConfig.direction === 'ascending') {
+                return <GoTriangleDown className='sort-icon' />;
+            } else if (sortConfig.direction === 'descending') {
+                return <GoTriangleUp className='sort-icon' />;
+            }
+        }
+        return <GoTriangleRight className='sort-icon' />;
+    };
+
     return (
         <div className='file-list-container'>
             <h2>파일 목록</h2>
@@ -54,15 +88,27 @@ export default function FileList({ files, selectedFileId, onFileSelect, onFileDe
                     <thead>
                         <tr>
                             <th></th>
-                            <th>파일 ID</th>
+                            <th onClick={() => handleSort('id')}>
+                                <div className="header-cell">
+                                    파일 ID {getSortIcon('id')}
+                                </div>
+                            </th>
                             <th>파일 이름</th>
-                            <th>업로드 날짜</th>
-                            <th>최종 수정일</th>
+                            <th onClick={() => handleSort('createTime')}>
+                                <div className="header-cell">
+                                    업로드 날짜 {getSortIcon('createTime')}
+                                </div>
+                            </th>
+                            <th onClick={() => handleSort('updateTime')}>
+                                <div className="header-cell">
+                                    최종 수정일 {getSortIcon('updateTime')}
+                                </div>
+                            </th>
                             <th>파일 삭제</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {files.map((file, index) => (
+                        {sortedFiles().map((file, index) => (
                             <tr key={index}>
                                 <td>
                                     <input
