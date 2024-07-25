8b39e930e98a19b63ae1b327b29d6747ab696960
codingosol <codingosol@gmail.com>
2024-07-25 15:24:15 +0900

    fileList Animation

--- src/component/DB/fileList/fileList.jsx
+++ src/component/DB/fileList/fileList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './fileList.css';
 import { FaTrash, FaEdit, FaSearch } from "react-icons/fa";
@@ -9,7 +9,10 @@ export default function FileList({ files, onFileSelect, fetchTables, onFileDelet
     const [editingFileId, setEditingFileId] = useState(null);
     const [newFileName, setNewFileName] = useState('');
     const [currentPage, setCurrentPage] = useState(1);
+    const [selectedFile, setSelectedFile] = useState(null);
     const [searchQuery, setSearchQuery] = useState('');
+    const fileListContainerRef = useRef(null);
+    const fileDetailsRef = useRef(null);
     const itemsPerPage = 15; // 페이지 당 항목 수
 
     const formatDate = (dateString) => {
@@ -42,21 +45,54 @@ export default function FileList({ files, onFileSelect, fetchTables, onFileDelet
             alert("파일 이름이 성공적으로 수정되었습니다.");
             setEditingFileId(null);
             fetchTables(); // Fetch the updated table list
+            setSelectedFile((prevFile) => ({ ...prevFile, fileName: newFileName })); // Update the selected file name
         } catch (error) {
             console.error("파일 이름 수정 중 오류 발생:", error);
             alert("파일 이름 수정 중 오류가 발생했습니다.");
         }
     };
 
-    const filteredFiles = () => {
-        if (!searchQuery) return files;
-        return files.filter(file => file.fileName.toLowerCase().includes(searchQuery.toLowerCase()));
+    const handleFileNameClick = (file, event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        setSelectedFile(file);
     };
 
-    const handleFileNameClick = (id) => {
-        onFileSelect(id);
-        fetchTables(id);
-    };
+    useEffect(() => {
+        if (selectedFile) {
+            setTimeout(() => {
+                if (fileListContainerRef.current) {
+                    fileListContainerRef.current.classList.add('expanded'); // Add expanded class
+                }
+                if (fileDetailsRef.current) {
+                    fileDetailsRef.current.classList.add('visible'); // Add visible class
+                }
+            }, 10); // 애니메이션이 자연스럽게 작동하도록 약간의 지연시간 추가
+        } else {
+            if (fileListContainerRef.current) {
+                fileListContainerRef.current.classList.remove('expanded'); // Remove expanded class
+            }
+            if (fileDetailsRef.current) {
+                fileDetailsRef.current.classList.remove('visible'); // Remove visible class
+            }
+        }
+    }, [selectedFile]);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                fileDetailsRef.current && !fileDetailsRef.current.contains(event.target) &&
+                fileListContainerRef.current && !fileListContainerRef.current.contains(event.target)
+            ) {
+                setSelectedFile(null);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     const handleNextPage = () => {
         if (currentPage * itemsPerPage < filteredFiles().length) {
@@ -68,96 +104,116 @@ export default function FileList({ files, onFileSelect, fetchTables, onFileDelet
         setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
     };
 
+    const handleViewTablesClick = () => {
+        if (selectedFile) {
+            fetchTables(selectedFile.id);
+        }
+    };
+
+    const filteredFiles = () => {
+        if (!searchQuery) return files;
+        return files.filter(file => file.fileName.toLowerCase().includes(searchQuery.toLowerCase()));
+    };
+
     const paginatedFiles = filteredFiles().slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
     const totalPages = Math.ceil(filteredFiles().length / itemsPerPage);
 
     return (
-        <div className='file-list-container'>
-            <h2>파일 목록</h2>
-            <div className="search-container">
-                <input
-                    type="text"
-                    placeholder="파일 이름으로 검색..."
-                    value={searchQuery}
-                    onChange={(e) => {
-                        setSearchQuery(e.target.value);
-                        setCurrentPage(1); // 검색할 때 첫 페이지로 이동
-                    }}
-                />
-                <FaSearch className="search-icon" />
-            </div>
-            {files && files.length === 0 ? (
-                <p>No files available.</p>
-            ) : (
-                <>
-                    <table className='table'>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>파일 이름</th>
-                                <th>업로드 날짜</th>
-                                <th>최종 수정일</th>
-                                <th>삭제/수정</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {paginatedFiles.map((file, index) => (
-                                <tr key={index}>
-                                    <td>{file.id}</td>
-                                    <td>
-                                        {editingFileId === file.id ? (
-                                            <div className="edit-name-wrapper">
-                                                <div className="edit-name-container">
-                                                    <input
-                                                        type="text"
-                                                        value={newFileName}
-                                                        onChange={(e) => setNewFileName(e.target.value)}
-                                                    />
-                                                </div>
-                                            </div>
-                                        ) : (
-                                            <div
-                                                className="file-name-container"
-                                                onClick={() => handleFileNameClick(file.id)}
-                                            >
-                                                <span className="file-name">
+        <div>
+            <div className="file-list-container" ref={fileListContainerRef}>
+                <h2>파일 목록</h2>
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="파일 이름으로 검색..."
+                        value={searchQuery}
+                        onChange={(e) => {
+                            setSearchQuery(e.target.value);
+                            setCurrentPage(1); // 검색할 때 첫 페이지로 이동
+                        }}
+                    />
+                    <FaSearch className="search-icon" />
+                </div>
+                <div className="file-list-content">
+                    <div className="file-list">
+                        {files && files.length === 0 ? (
+                            <p>No files available.</p>
+                        ) : (
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th className='header-cell'>
+                                            파일 이름
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {paginatedFiles.map((file, index) => (
+                                        <tr key={index} onClick={(e) => handleFileNameClick(file, e)} className={selectedFile && selectedFile.id === file.id ? 'selected' : ''}>
+                                            <td>
+                                                <div className="file-name">
                                                     {file.fileName}
-                                                </span>
-                                            </div>
-                                        )}
-                                    </td>
-                                    <td>{formatDate(file.createTime)}</td>
-                                    <td>{formatDate(file.updateTime)}</td>
-                                    <td>
-                                        <button className='trash-icon' onClick={() => onFileDelete(file.id)}>
-                                            <FaTrash />
-                                        </button>
-                                        {editingFileId === file.id ? (
-                                            <IoIosSave
-                                                onClick={() => handleSaveFileName(file)}
-                                                className='save-file-button'
-                                            />
-                                        ) : (
-                                            <FaEdit
-                                                onClick={(e) => {
-                                                    e.stopPropagation();
-                                                    handleEditFileName(file);
-                                                }}
-                                                className='edit-file-button'
-                                            />
-                                        )}
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                    <div className="pagination">
-                        <GoTriangleLeft onClick={handlePreviousPage} disabled={currentPage === 1} className='pagination-icon' />
-                        <span>{currentPage}</span>
-                        <GoTriangleRight onClick={handleNextPage} disabled={currentPage === totalPages} className='pagination-icon' />
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        )}
+                        <div className="pagination">
+                            <GoTriangleLeft onClick={handlePreviousPage} disabled={currentPage === 1} className="pagination-icon" />
+                            <span>{currentPage}</span>
+                            <GoTriangleRight onClick={handleNextPage} disabled={currentPage === totalPages} className="pagination-icon" />
+                        </div>
                     </div>
-                </>
-            )}
+                </div>
+                <button className="table-view-button" onClick={handleViewTablesClick} disabled={!selectedFile}>테이블 목록 보기</button>
+            </div>
+            <div className={`file-details ${selectedFile ? 'visible' : ''}`} ref={fileDetailsRef}>
+                {selectedFile && (
+                    <div className='detail-container'>
+                        <h3>파일 상세 정보</h3>
+                        <p><strong>파일 ID:</strong> {selectedFile.id}</p>
+                        <p><strong>파일 이름:</strong> {selectedFile.fileName}</p>
+                        <p><strong>업로드 날짜:</strong> {formatDate(selectedFile.createTime)}</p>
+                        <p><strong>최종 수정일:</strong> {formatDate(selectedFile.updateTime)}</p>
+                        <div className="file-action-buttons">
+                            <div className="edit-name-wrapper">
+                                <div className="edit-name-container">
+                                    <span>파일 이름 수정</span>
+                                    {editingFileId === selectedFile.id ? (
+                                        <>
+                                            <input
+                                                type="text"
+                                                value={newFileName}
+                                                onChange={(e) => setNewFileName(e.target.value)}
+                                            />
+                                            <IoIosSave
+                                                onClick={() => handleSaveFileName(selectedFile)}
+                                                className="save-file-button"
+                                            />
+                                        </>
+                                    ) : (
+                                        <FaEdit
+                                            onClick={(e) => {
+                                                e.stopPropagation();
+                                                handleEditFileName(selectedFile);
+                                            }}
+                                            className="edit-file-button"
+                                        />
+                                    )}
+                                </div>
+                            </div>
+                            <div className="delete-file-container">
+                                <span>파일 삭제</span>
+                                <button className="trash-icon" onClick={() => onFileDelete(selectedFile.id)}>
+                                    <FaTrash />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
