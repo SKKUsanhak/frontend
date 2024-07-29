882a75c05bd28494c3dd26476c6e16b2f3eaeb4f
codingosol <codingosol@gmail.com>
2024-07-29 13:38:58 +0900

    Home UI

--- src/component/Home/home.jsx
+++ src/component/Home/home.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './home.css';
+import { MdOutlineUploadFile } from "react-icons/md";
+import { TbDatabase } from "react-icons/tb";
 
 export default function Home() {
     return (
         <div className="home-container">
             <div className="auth-buttons">
-                <Link to="/login" className="button">로그인</Link>
-                <Link to="/register" className="button">회원가입</Link>
+                <Link to="/login" className="auth-button">로그인</Link>
+                <Link to="/register" className="auth-button">회원가입</Link>
             </div>
             <div className="main-content">
-                <Link to="/upload" className='button' />
-                <Link to="/database" className='button' />
+                <div className="main-button">
+                    <Link to="/upload" className="icon-1">
+                        <MdOutlineUploadFile className="icon" />
+                        <span className="text">Upload</span>
+                    </Link>
+                </div>
+                <div className="main-button">
+                    <Link to="/database" className="icon-2">
+                        <TbDatabase className="icon" />
+                        <span className="text">Database</span>
+                    </Link>
+                </div>
             </div>
         </div>
     );
