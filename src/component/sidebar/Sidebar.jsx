ceb4f3987181f8c3d41b8b3a998e3fd13b765d8a
codingosol <codingosol@gmail.com>
2024-08-07 15:41:11 +0900

    Sidebar amend

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -1,31 +1,46 @@
 import React, { useState } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
-import { BsLayoutTextSidebarReverse } from "react-icons/bs";
+import { NavLink } from 'react-router-dom';
+import { IoMenu } from "react-icons/io5";
 import './Sidebar.css';
+import { MdDomainAdd } from "react-icons/md";
+import { BiBuildings } from "react-icons/bi";
+import { FaRegFileAlt } from "react-icons/fa";
+import { FiTable } from "react-icons/fi";
+import { CgDatabase } from "react-icons/cg";
 
 const Sidebar = ({ onToggle }) => {
     const [isCollapsed, setIsCollapsed] = useState(false);
-    const location = useLocation();
 
     const toggleSidebar = () => {
         setIsCollapsed(!isCollapsed);
         onToggle(!isCollapsed); // 부모 컴포넌트에 상태 전달
     };
 
-    // 경로에 따라 활성화 상태를 결정하는 함수
-    const isLinkActive = (path) => {
-        return location.pathname.startsWith(path);
-    };
-
     return (
         <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
-            <BsLayoutTextSidebarReverse className="sidebar-icon" onClick={toggleSidebar} />
+            <IoMenu className="sidebar-icon" onClick={toggleSidebar} />
             {!isCollapsed && (
                 <div className="sidebar-links">
-                    <NavLink to="/" className="sidebar-link" activeClassName="active">Home</NavLink>
-                    <NavLink to="/buildings" className={`sidebar-link ${isLinkActive('/buildings') ? 'active' : ''}`}>건물 목록</NavLink>
-                    <NavLink to="/buildings/:buildingId/files" className={`sidebar-link ${isLinkActive('/buildings') && isLinkActive('/files') ? 'active' : 'disabled'}`}>파일 목록</NavLink>
-                    <NavLink to="/buildings/:buildingId/files/:fileId/tables" className={`sidebar-link ${isLinkActive('/buildings') && isLinkActive('/files') && isLinkActive('/tables') ? 'active' : 'disabled'}`}>테이블 목록</NavLink>
+                    <div className="sidebar-item">
+                        <MdDomainAdd className="sidebar-link-icon"/>
+                        <NavLink to="/buildings/upload" className="sidebar-link" activeClassName="active">Upload</NavLink>
+                    </div>
+                    <div className="sidebar-item">
+                        <BiBuildings className="sidebar-link-icon"/>
+                        <NavLink to="/buildings" className="sidebar-link" activeClassName="active">Building</NavLink>
+                    </div>
+                    <div className="sidebar-item">
+                        <FaRegFileAlt className="sidebar-link-icon"/>
+                        <NavLink to="/buildings/:buildingId/files" className="sidebar-link" activeClassName="active">File</NavLink>
+                    </div>
+                    <div className="sidebar-item">
+                        <FiTable className="sidebar-link-icon"/>
+                        <NavLink to="/buildings/:buildingId/files/:fileId/tables" className="sidebar-link" activeClassName="active">Table</NavLink>
+                    </div>
+                    <div className="sidebar-item">
+                        <CgDatabase className="sidebar-link-icon"/>
+                        <NavLink to="/buildings/:buildingId/files/:fileId/tables/:tableId/datas" className="sidebar-link" activeClassName="active">Data</NavLink>
+                    </div>
                 </div>
             )}
         </div>
